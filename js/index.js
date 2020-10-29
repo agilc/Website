7d7dc0255293a8617395c55eb007f00ee6f721bc
agilc <agilc315@gmail.com>
2020-10-29 21:47:50 +0530

    mobile responsive

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
-  var app = document.getElementById('running_text');
+    var app = document.getElementById('running_text');
 
-  var typewriter = new Typewriter(app, {
-      loop: true
-  });
+    var typewriter = new Typewriter(app, {
+        loop: true
+    });
 
-  typewriter.typeString('I am Agil C')
+    typewriter.typeString('I am Agil C')
       .pauseFor(2500)
       .deleteChars(6)
       .typeString('a Full Stack Engineer!')
@@ -15,5 +15,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
       // .typeString('<strong>altered!</strong>')
       // .pauseFor(2500)
       .start();
+
+    $('.scroll-down-arrow').on("click", function(){
+        const elmnt = document.getElementById("blogs");
+        elmnt.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    });
+
+    $('.left-more-icon').on("click", function(){
+        $('.sidebar-wrapper').toggle();
+    });
 
 })
